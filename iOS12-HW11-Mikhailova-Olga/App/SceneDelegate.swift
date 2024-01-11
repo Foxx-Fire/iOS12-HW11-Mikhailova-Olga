44e1260f6633e36aa0f02a0dfc25e721ead82531
Olga Mikhailova <foxx_fire@mail.ru>
2024-01-11 21:11:39 +0300

    Settings

---
+++ iOS12-HW11-Mikhailova-Olga/App/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  iOS12-HW11-Mikhailova-Olga
+//
+//  Created by mbpro2.0/16/512 on 11.01.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+    
+}
